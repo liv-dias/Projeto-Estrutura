b52c0baffd9b387c1e76b23ed47e7c22c345896a
Beatriz A. Siqueira <biasiqueira602@gmail.com>
2026-05-19 11:24:58 +0000

    recoloquei os arquivos

--- src/apl2/NodeOriginal.java
+++ src/apl2/NodeOriginal.java
@@ -1,170 +1,73 @@
 //******************** ATENÇÃO! *********************
 // O conteúdo deste arquivo não deve ser modificado!
 //******************** ATENÇÃO! *********************
-// arquivo: src/apl2/LinkedListOriginal.java
+// arquivo: src/apl2/NodeOriginal.java
 
 package apl2;
 
-public class LinkedListOriginal {
+public class NodeOriginal {
 	
-	private NodeOriginal head;
-	private NodeOriginal tail;
-	private int count;
+	private int id;
+	private String nome;
+	private int inteiro;
+	private int decimo;
+	private NodeOriginal next;
 	
-	public LinkedListOriginal() {
-		head = null;
-		tail = null;
-		count = 0;
+	public NodeOriginal() {
+		this(-1, "", 0, 0, null);
 	}
 	
-	public void destroy() {
-		clear();
+	public NodeOriginal(int id, String nome, int inteiro, int decimo, NodeOriginal next) {
+		this.id = id;
+		this.nome = nome;
+		this.inteiro = inteiro;
+		this.decimo = decimo;
+		this.next = next;
 	}
 	
-	public void insert(int id, String nome, int inteiro, int decimo) {
-		NodeOriginal node = new NodeOriginal(id, nome, inteiro, decimo, head);
-		
-		if (isEmpty()) {
-			tail = node;
-		}
-		
-		head = node;
-		++count;
+	public int getId() {
+		return id;
 	}
 	
-	public void append(int id, String nome, int inteiro, int decimo) {
-		NodeOriginal node = new NodeOriginal(id, nome, inteiro, decimo, null);
-		
-		if (isEmpty()) {
-			head = node;
-		} else {
-			tail.setNext(node);
-		}
-		
-		tail = node;
-		++count;
+	public void setId(int id) {
+		this.id = id;
 	}
 	
-	public NodeOriginal removeHead() {
-		if (isEmpty()) {
-			return null;
-		}
-		
-		NodeOriginal toRemove = head;
-
-		head = head.getNext();
-		--count;
-		
-		if (isEmpty()) {
-			tail = null;
-		}
-		
-		toRemove.setNext(null);
-		return toRemove;
+	public String getNome() {
+		return nome;
 	}
 	
-	public NodeOriginal removeTail() {
-		if (head == tail)
-			return removeHead();
-		
-		NodeOriginal toRemove = head;
-		NodeOriginal previous = null;
-		while (toRemove != tail) {
-			previous = toRemove;
-			toRemove = toRemove.getNext();
-		}
-		
-		tail = previous;
-		tail.setNext(null);
-		--count;
-		
-		toRemove.setNext(null);
-		return toRemove;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	
-	public NodeOriginal removeNode(int id) {
-		NodeOriginal toRemove = head;
-		NodeOriginal previous = null;
-		while (toRemove != null && toRemove.getId() != id) {
-			previous = toRemove;
-			toRemove = toRemove.getNext();
-		}
-		
-		if (toRemove == null) {
-			return null;
-		}
-		
-		if (toRemove == head) {
-			return removeHead();
-		}
-		
-		if (toRemove == tail) {
-			return removeTail();
-		}
-		
-		previous.setNext(toRemove.getNext());
-		--count;
-		
-		toRemove.setNext(null);		
-		return toRemove;
+	public int getInteiro() {
+		return inteiro;
+	}
+	
+	public void setInteiro(int inteiro) {
+		this.inteiro = inteiro;
+	}
+	
+	public int getDecimo() {
+		return decimo;
+	}
+	
+	public void setDecimo(int decimo) {
+		this.decimo = decimo;
 	}
 
-	public NodeOriginal getHead() {
-		return head;
+	public NodeOriginal getNext() {
+		return next;
 	}
 	
-	public NodeOriginal getTail() {
-		return tail;
+	public void setNext(NodeOriginal next) {
+		this.next = next;
 	}
 	
-	public NodeOriginal getNode(int id) {
-		NodeOriginal node = head;
-		while (node != null) {
-			if (node.getId() == id) {
-				return node;
-			}
-			node = node.getNext();
-		}
-		
-		return null;
-	}
-	
-	public int count() {
-		return count;
-	}
-	
-	public boolean isEmpty() {
-		return head == null;
-	}
-	
-	public void clear() {
-		while (!isEmpty()) {
-			removeHead();
-		}
-	}
-
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append("(" + count + ") \n");
-		
-		NodeOriginal node = head;
-		while (node != null) {
-			sb.append("(")
-			.append(node.getId())
-			.append(" # ")
-			.append(node.getNome())
-			.append(" # ")
-			.append(node.getInteiro())
-			.append(" # ")
-			.append(node.getDecimo())
-			.append(") -> \n");
-			node = node.getNext();
-		}
-		sb.append("null.");
-		
-		return sb.toString();
+		return "[dados: (" + id + ";" + nome + ";" + inteiro + ";" + decimo + ") | next: " + next + "]";
 	}
 
 }
